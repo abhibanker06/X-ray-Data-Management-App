99be015e015f7120e5fe096d4488557eb26db05d
Abhishek <abhishekbanker971@gmail.com>
2026-02-28 19:38:38 +0530

    changed port code

--- backend/app.js
+++ backend/app.js
@@ -19,6 +19,7 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
 app.use("/api/categories", categoryRoutes)
 app.use("/api/images", imageRoutes)
 
-app.listen(5000,()=>{
-    console.log("Server is running on port 5000");
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
